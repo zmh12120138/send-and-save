c7b3f11a3615f930c6188b9c9c5b41c5514e990a
zmh12120138 <zmhshu@live.cn>
2015-07-14 16:02:58 +0800

    send and save

---
+++ js/server-parse（老版，独立解析）.js
@@ -0,0 +1,52 @@
+var parse=require('./js/parse.js');  //引入自定义的parse.js模块
+var cluster=require('cluster');  //引入cluster模块
+var settings=require('./settings.js');  //引入自定义的setting.js模块
+var mysql=require('mysql');  //引入Mysql模块
+var fs = require('fs');  //引入fs模块
+var os=require('os');   //引入os模块
+var numCPUs=os.cpus().length;   //获取CPU的数量
+var workers={};
+var connection=mysql.createConnection({host:settings.host,port:settings.port,database:settings.database,user:settings.user,password:settings.password});
+//连接mysql数据库，设置信息在settings.js模块
+
+if(cluster.isMaster){
+    //主进程分支
+    //初始开启与CPU数量相同的工作进程
+    for(var i=0;i<numCPUs;i++){
+        var worker=cluster.fork();
+        workers[worker.pid]=worker;
+    }
+
+    cluster.on('exit',function(worker,code,signal){  //当一个进程关闭后，重新开启一个进程
+        delete workers[worker.pid];
+        worker = cluster.fork();
+        workers[worker.pid] = worker;
+        if(worker.suicide===true){
+            console.log('子进程%d自动退出',worker.id+'已开启新的子进程');
+        }
+        else{
+            console.log('子进程%d异常退出,已开启新的子进程，退出代码为%d',worker.id,code);
+        }
+        if(signal){
+            console.log('退出信号为%s',signal);
+        }
+    })
+    //在父进程中从数据库中读取原始数据，并发送给子进程
+    connection.query('SELECT * FROM originaldata',function(err,result){
+        if(err) throw (err);
+        workers[worker.pid].send(result);
+    })
+
+}else {
+    //子进程收到父进程发送的数据
+    process.on('message', function (result) {
+        //遍历父进程传过来的对象数组result
+        for (var i = 0; i < result.length; i++) {
+            var afterparse = parse.parseAll(result[i].originaldata); //并调用parse模块中的parseAll方法解析数据
+            connection.query('INSERT INTO test1 SET ?', {meterid: afterparse.meterid, cold: afterparse.cold, warm: afterparse.warm, power: afterparse.power, flow: afterparse.flow, flowacc: afterparse.flowacc, temwatersupply: afterparse.temwatersupply, temwaterreturn: afterparse.temwaterreturn, worktime: afterparse.worktime, metertime: afterparse.metertime, status: afterparse.status}, function (err, result) {
+                if (err) throw (err);
+                else console.log('本条记录由子进程' + cluster.worker.id + '解析完毕');
+            });  //将解析后的数据存储至数据库
+        }
+    });
+}
--- server-transfer.js
+++ server-transfer.js
@@ -6,6 +6,7 @@ var mysql=require('mysql');  //引入Mysql模块
 var app=express();   //创建express实例
 var parse=require('./js/parse.js');  //引入自定义的parse.js模块
 var cluster=require('cluster');  //引入cluster模块
+var cp=require('child_process');
 var os=require('os');   //引入os模块
 var morgan=require('morgan');  //引入morgon模块---用于存储日志
 var fs = require('fs');  //引入fs模块
@@ -25,22 +26,32 @@ app.use(function(err, req, res, next){
     next();
 });     //将错误日志写入errTransfer.log中
 if(cluster.isMaster){
-    //主进程分支
-    cluster.on('death', function (worker) {    // 当一个工作进程结束时，重启工作进程
-    delete workers[worker.pid];
-    worker = cluster.fork();
-    workers[worker.pid] = worker;
-    });
     //初始开启与CPU数量相同的工作进程
     for(var i=0;i<numCPUs;i++){
         var worker=cluster.fork();
         workers[worker.pid]=worker;
     }
+    //主进程分支
+
+    cluster.on('exit',function(worker,code,signal){   //当一个进程关闭后，重新开启一个进程
+        delete workers[worker.pid];
+        worker = cluster.fork();
+        workers[worker.pid] = worker;
+        if(worker.suicide===true){
+            console.log('子进程%d自动退出',worker.id+'已开启新的子进程');
+        }
+        else{
+            console.log('子进程%d异常退出,已开启新的子进程，退出代码为%d',worker.id,code);
+        }
+        if(signal){
+            console.log('退出信号为%s',signal);
+        }
+    })
 }else{
     //工作进程分支，启动服务器
     server.listen(1337);
     var socket=sio.listen(server);   //监听1337端口
-
+    var parse= cp.fork(__dirname+'/server-parse.js'); //再次开启子进程，运行sever-parse模块
     var connection=mysql.createConnection({host:settings.host,port:settings.port,database:settings.database,user:settings.user,password:settings.password});
    //连接mysql数据库，设置信息在settings.js模块
     socket.on('connection',function(socket){
@@ -48,13 +59,17 @@ if(cluster.isMaster){
         console.log('与客户端的传输通道建立');
         socket.on('send',function(data){
             //监听send事件
+            parse.send(data.send);  //向名字为parse的子进程发送数据，数据为data.send
             connection.query('INSERT INTO originaldata SET ?',{originaldata:data.send,date:new Date()},function(err,result){
                 if(err){
                     errLogStream.write( '[' + new Date() + '] ' + '\n' + err.stack + '\n');
                     throw (err);
                 }
-                //向控制台发送信息
-                console.log('数据接收完毕！  任务编号:'+data.code+'由子进程'+cluster.worker.id+'处理');
+                else{
+                    //向控制台发送信息
+                    console.log('数据接收完毕！  任务编号:'+data.code+'由子进程'+cluster.worker.id+'处理');
+
+                }
             });//将接收到的原始数据存至数据库
         });
 
@@ -62,5 +77,6 @@ if(cluster.isMaster){
             //监听disconnect事件
             console.log('已经断开传输通道连接！');
         })
+
     });
 }
